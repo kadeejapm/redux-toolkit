7551069317735cbf87179f5b3a462640846b9d4e
kadeejapm <pmkadeeja562@gmai.com>
2023-12-07 12:54:12 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Task from "./components/Task";
+import TaskList from "./components/TaskList";
+import './index.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Task />
+      <TaskList />
+    </div>
+  );
+}
+
+export default App;
+
+
